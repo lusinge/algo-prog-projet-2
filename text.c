fa5251a76db7306e3b52fa1fdcf9211ea2867227
Lucien <lucien.gheerbrant@etu.emse.fr>
2023-04-09 11:07:24 +0200

    Add: Prefix cmp
    If a word has the prefix entered, return true

---
+++ text.c
@@ -0,0 +1,20 @@
+#include "text.h"
+
+char* cut_str(char* str)
+{
+	char* result = (char*) malloc(sizeof(char) * MAX_WORD_LENGTH);
+	int len = strlen(str);
+	int i = 0;
+
+	while(i < len && str[i] != ' ' && i < MAX_WORD_LENGTH - 1) {
+		result[i] = str[i];
+		i++;
+	}
+	result[i] = '\0';
+	return result;
+}
+
+bool has_prefix(char* word, char* prefix)
+{
+    return strncmp(word, prefix, strlen(prefix)) == 0;
+}
