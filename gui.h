0dd3355e781cdb88f19cbf579b7ca2cc0d7d0bbf
lulu <lucien.gheerbrant@etu.emse.fr>
2023-04-15 15:43:45 +0200

    Fix: AppData Struct & Gtk window
    Put the main window in the appdata struct so i can pass it into the pop
    up dialogs

--- gui.h
+++ gui.h
@@ -4,18 +4,21 @@
 
 typedef struct {
   GtkApplication* app;
+  GtkWidget* window;
   char search_text[MAX_WORD_LENGTH];
   HashTable* hashTab;
   GtkListBox* suggestion_list;
+  GtkWidget* word_entry;
 } AppData;
 
-static void on_search_changed(GtkSearchEntry *entry, gpointer user_data);
-static void on_search_button_clicked(GtkButton *button, gpointer user_data);
-static void activate_cb(GtkApplication *app, gpointer user_data);
-static void update_suggestion_list(AppData *data, TopWord topThreeWords[3]);
 int search_window(int argc, char *argv[], AppData *data);
-static void on_add_word_dialog_response(GtkDialog *dialog, gint response_id, gpointer user_data);
+static void activate_cb(GtkApplication *app, gpointer user_data);
+static void on_search_button_clicked(GtkButton *button, gpointer user_data);
+static void on_search_changed(GtkSearchEntry *entry, gpointer user_data);
+static void update_suggestion_list(AppData *data, TopWord topThreeWords[3]);
+static void on_row_activated(GtkListBox *list_box, GtkListBoxRow *row, gpointer user_data);
+static void show_add_word_dialog(gpointer user_data);
+static void on_add_word_dialog_response(GtkDialog *dialog, gint response, gpointer user_data);
 static void on_remove_word_dialog_response(GtkDialog *dialog, gint response_id, gpointer user_data);
-static void show_add_word_dialog(GtkWidget *parent, gpointer user_data);
-static void show_remove_word_dialog(GtkWidget *parent, gpointer user_data);
+static void show_remove_word_dialog(gpointer user_data);
 void removeWord(HashTable* hashTab, char* word, char* file_name);
